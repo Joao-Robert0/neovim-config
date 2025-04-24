6243083733f9d8ffca9e4b274b4f2428d0cd4676
João Roberto <joao.roberto@ufv.br>
2025-04-24 13:46:01 -0300

    plugins do catppuccin e do vim-tmux navigator adicionados

---
+++ lua/kickstart/plugins/catppuccin.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    'catppuccin/nvim',
+    priority = 1000,
+    name = 'catppuccin',
+    config = function()
+      require('catppuccin').setup {
+        flavour = 'mocha',
+        transparent_background = true,
+      }
+      vim.cmd.colorscheme 'catppuccin'
+    end,
+  },
+}
